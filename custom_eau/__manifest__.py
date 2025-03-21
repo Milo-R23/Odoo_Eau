35a4c579800ae3a3299f3bc8007c2e9f8f874c9d
milo.racz <milo.racz@axis.local>
2025-03-21 14:13:42 +0100

    Icon hinzugefügt & Manifest aktualisiert

--- custom_eau/__manifest__.py
+++ custom_eau/__manifest__.py
@@ -4,11 +4,12 @@
     'summary': 'Automatische eAU-Abfrage für Odoo',
     'description': 'Fügt einen Button hinzu, um Krankmeldungen automatisch abzurufen.',
     'category': 'Human Resources',
-    'author': 'Dein Name',
+    'author': 'Milo Racz',
     'depends': ['hr'],
     'data': [
         'views/employee_view.xml',
     ],
+    'images': ['static/description/icon.png'],
     'installable': True,
-    'application': False,
+    'application': True,
 }
